5cc9be23d2455f208d4f335599cb97d148bc55cc
admin account <brendan@curacaowebhosting.com>
2016-01-14 23:48:12 +0200

    added method decodeCookie.

--- src/Toolbox/Library/Session/CookieService.php
+++ src/Toolbox/Library/Session/CookieService.php
@@ -74,4 +74,30 @@ class CookieService
         }
     }
 
+    /**
+     * Returns a cookie as an array
+     * @param string $name
+     * @return mixed|null
+     */
+    function decodeCookie($name = 'referrer')
+    {
+        $cookie = $this->getCookie($name);
+
+        /**
+         * Test and extract the cookie
+         */
+        if (is_object(json_decode($cookie)))
+        {
+            $cookie = json_decode($cookie,true);
+        }
+
+        if (is_object(json_decode($cookie['data'])))
+        {
+            $cookie['data'] = json_decode($cookie['data'] , true);
+        }
+
+        return $cookie;
+
+    }
+
 }
