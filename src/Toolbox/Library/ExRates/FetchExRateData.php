b553b89b6c87033ab61bcc5f1a7bffc9162a250e
admin account <brendan@curacaowebhosting.com>
2016-01-16 02:01:27 +0200

    added ZA fx.

--- src/Toolbox/Library/ExRates/FetchExRateData.php
+++ src/Toolbox/Library/ExRates/FetchExRateData.php
@@ -12,8 +12,9 @@ class FetchExRateData
     public function getExRateMethods()
     {
         return [
-            1 => 'TMC EUR',
-            2 => 'Google EUR'
+            1 => 'EUR: TMR',
+            2 => 'EUR: Google Docs',
+            3 => 'ZAR: fxexchangerate.com'
         ];
     }
 
@@ -34,6 +35,9 @@ class FetchExRateData
                 break;
             case 2:
                 return $this->getExRateEurArray2();
+                break;
+            case 3:
+                return $this->getExRateZarArray1();
                 break;
             default:
                 return [];
@@ -132,6 +136,62 @@ class FetchExRateData
             $newArray[$row['title']]['to'] = $row['title'];
             $rate = explode( ' ' , $row['description']);
             $newArray[$row['title']]['rate'] = $rate[1];
+            $newArray[$row['title']]['description'] = $row['description'];
+        }
+
+        return $newArray;
+
+    }
+
+    /**
+     * Returns a nicely formatted array from the feed URL
+     * http://currencyfeed.com/
+     *
+     * @param string $url
+     * @return array
+     *
+     * 'AED' =>
+     *    array (size=4)
+     *    'from' => string 'EUR' (length=3)
+     *    'to' => string 'AED' (length=3)
+     *    'rate' => string '4.74487' (length=7)
+     *    'description' => string '1 Euro = 4.74487 United Arab Emirates Dirham' (length=44)
+     *
+     *
+     */
+    private function getExRateZarArray1( $url = "http://zar.fxexchangerate.com/rss.xml" )
+    {
+        //Grab data from the feed
+        $xml=@simplexml_load_file($url);
+
+        //If the feed is unreachable it will be false
+        if ( ! $xml ) {
+            return [];
+        }
+
+        $json = json_encode($xml);
+        $exRateArray = json_decode($json,true);
+        $newArray = [];
+
+        if (!isset($exRateArray['channel']['item']))
+        {
+            return [];
+        }
+
+        foreach ( $exRateArray['channel']['item'] AS $key => $row )
+        {
+            if ( (!isset($row['title'])) OR (!isset($row['description'])) )
+            {
+                continue;
+            }
+
+            $str = $row['title'];
+            $del = ['(',')'];
+            $from_to = explode( $del[0], str_replace($del, $del[0], $str) );
+            $newArray[$row['title']]['from'] = $from_to[1];
+            $newArray[$row['title']]['to'] = $from_to[3];
+            $rate = explode( ' ' , $row['description']);
+            $newArray[$row['title']]['rate'] = $rate[5];
             $newArray[$row['title']]['description'] = $row['description'];
         }
 
